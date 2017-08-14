1b5ef0e417790d7532881e43b90b02eb93c961d5
97387 <97387406@qq.com>
2017-08-14 19:59:10 +0800

    风尚大典

---
+++ app/src/main/java/jiyun/com/gitdemo/MainActivity.java
@@ -0,0 +1,11 @@
+package jiyun.com.gitdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
